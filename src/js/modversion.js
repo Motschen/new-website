ce144849f3fcb6e3269f302c976ec873cba890f8
Martin Prokoph <motschen@midnightdust.eu>
2024-02-08 21:50:07 +0100

    More progress

--- src/js/modversion.js
+++ src/js/modversion.js
@@ -4,18 +4,29 @@ var finishedArray = modInfo.reduce(function(map, value) {
   value.game_versions.forEach(version => {
     value.loaders.forEach(loader => {
       const loaderAndVersion = loader + "+" +version;
-      if (Object.values(map).includes(loaderAndVersion)) {
-        console.log("return")
-        return;
+      if (!Object.values(map).includes(loaderAndVersion)) {
+        if (value.featured) map[loaderAndVersion] = value.id;
       }
-      if (value.featured) map[loaderAndVersion] = value.id;
     });
   });
   return map;
 }, {});
-console.log(finishedArray);
 export var selectedLoader = "fabric";
+
+export var versionList = [];
+var versionMap = await Object.keys(finishedArray).reduce(function(map, value) { 
+    if (value.startsWith(selectedLoader)) {
+        const version = value.split("+")[1];
+        if (!Object.values(map).includes(version)) {
+            map[version] = "";
+        }
+    }
+    return map;
+}, {});
+versionList = await Object.keys(versionMap).sort().reverse();
+
 export var selectedVersion = "1.20.4";
+
 var resultingVersionID = await finishedArray[selectedLoader + "+" + selectedVersion];
 export var resultingVersion = "Could not find the latest version!";
 resultingVersion = modInfo.find((info) => info.id == resultingVersionID).version_number;
