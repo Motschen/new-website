d7159cfc9ba3d02149a631751a0c0894d0fbe711
Martin Prokoph <motschen@midnightdust.eu>
2024-02-09 19:14:36 +0100

    WWW

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://midnightdust.eu',
+  site: 'https://www.midnightdust.eu',
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
