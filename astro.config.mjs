7a1d9ac69ab592c0cc7bf4de9c2ed435fb7ba9c7
Martin Prokoph <motschen@midnightdust.eu>
2024-02-09 19:24:49 +0100

    Compress images and disable automatic compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
 import icon from "astro-icon"
 
 // https://astro.build/config
@@ -10,5 +9,5 @@ export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
-  }), compress()],
+  })],
 })
